055de0baaaa0678388c3a3dd01b54b9d4d118b62
bibi <kevinfiguereo40@gmail.com>
2025-03-16 23:18:48 -0400

    Agregar botones principales

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-import tkinter
+"""
+   Este archivo es el punto de entrada del programa, es decir, la ventana principal
+"""
 
-def main():
-	principal = tkinter.Tk()
-	principal.title("Seguro ARS")
-	principal.mainloop()
+# Importa la libreria necesaria para interfaces graficas
+import tkinter as tk
 
-if __name__ == '__main__':
-	main()
+programaPrincipal = tk.Tk()
+programaPrincipal.title("Seguros ARS")
+agregar_usuario = tk.Button(programaPrincipal, text = "Agregar Usuario")
+agregar_usuario.pack()
+ver_usuarios = tk.Button(programaPrincipal, text = "Ver usuarios")
+ver_usuarios.pack()
+
+
+
+programaPrincipal.mainloop()
