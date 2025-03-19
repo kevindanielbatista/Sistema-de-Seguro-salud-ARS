369747d6d669145e62d0ad8e4e538927b929be14
bibi <kevinfiguereo40@gmail.com>
2025-03-18 20:46:59 -0400

    Quitar comentarios innecesarios

--- main.py
+++ main.py
@@ -9,23 +9,3 @@ import VentanaPrincipal as vp
 if __name__ == "__main__":
    ventanaInicial = vp.VentanaPrincipal() # Crear objeto ventana principal
    ventanaInicial.mainloop()
-
-# #Crear boton de gestionar citas medicas
-# citas = tk.Button(programaPrincipal, text = "Gestionar Citas")
-# citas.pack()
-
-# #Crear boton de gestionar seguros
-# seguros = tk.Button(programaPrincipal, text = "Gestionar seguros")
-# seguros.pack()
-
-# #Crear boton de reportes
-# reportes = tk.Button(programaPrincipal, text = "Reportes")
-# reportes.pack()
-
-# #Crear boton de configuracion
-# configuracion = tk.Button(programaPrincipal, text = "Configuracion")
-# configuracion.pack()
-
-
-
-# programaPrincipal.mainloop()
