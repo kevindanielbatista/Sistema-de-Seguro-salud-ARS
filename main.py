ab030dde400e3ce2edad585c8f11d4a849bcdf55
bibi <kevinfiguereo40@gmail.com>
2025-04-10 11:34:22 -0400

    Cambiar mainloop a exec

--- main.py
+++ main.py
@@ -4,8 +4,16 @@
 
 #Importa la libreria necesaria para interfaces graficas
 
+import sys
+from PyQt6.QtWidgets import (QApplication, QMainWindow, QWidget, 
+                            QPushButton, QVBoxLayout, QLabel)
 import VentanaPrincipal as vp 
 
 if __name__ == "__main__":
+   aplicacion = QApplication([])
+
    ventanaInicial = vp.VentanaPrincipal() # Crear objeto ventana principal
-   ventanaInicial.mainloop()
+   ventanaInicial.show()
+
+   aplicacion.exec()
+
