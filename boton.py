fae76b3d3648bfea32706222ca622abcca0bc8df
bibi <kevinfiguereo40@gmail.com>
2025-04-10 11:33:49 -0400

    Eliminar por cambio de librería

--- boton.py
+++
@@ -1,8 +0,0 @@
-import customtkinter as grafica
-class Boton(grafica.CTkButton):
-	def __init__(self, texto, master = None, *args, **kwargs):
-		super().__init__(master = master, text = texto, *args, **kwargs)
-
-	
-		
-
