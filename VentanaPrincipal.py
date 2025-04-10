389c9267fca6b2bb3d4de34d35c9247f87b32384
bibi <kevinfiguereo40@gmail.com>
2025-04-10 11:32:31 -0400

    Cambiar libreria a PYQT

--- VentanaPrincipal.py
+++ VentanaPrincipal.py
@@ -1,43 +1,29 @@
-import customtkinter as grafica
-import inicio_sesion as inicio
-import funciones_utilidad as utilidad
+import sys
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import (QApplication, QMainWindow, QWidget, 
+                            QPushButton, QVBoxLayout, QLabel)
+import panel as pn
 
-
-# Crear clase VentanaPrincipal que hereda la clase Tk. TK representa el punto de entrada, solo puede haber una.
-class VentanaPrincipal(grafica.CTk):
-    #self es un parametro obligatorio que se refiere al objeto siendo creado.
-    def __init__(self):  # Declarar constructor
-        #invocar al constructor de una clase madre a partir de una clase hija
+class VentanaPrincipal(QMainWindow):
+    def __init__(self):
         super().__init__()
 
-        # dar tamaño a la ventana
-        self.geometry('400x400')
+        self.setWindowTitle("Seguros ARS")
 
-        # Dar titulos
-        self.wm_title("Seguros ARS")
+        #Referencia a la ventana Panel
+        self.panel_referencia = None
 
-        #Crea una variable que almacene el cuadro. None cuando no exista
-        self._cuadro = None
+        #disposicion de los botones
+        self.arreglo = QVBoxLayout()
 
-        self.cambiar_cuadro(clase_cuadro = inicio.InicioSesion)
+        #Boton de cambio de pantalla
+        self.boton = QPushButton("Cambio test")
+        self.boton.clicked.connect(self.mostrar_ventana_panel)
+        self.setCentralWidget(self.boton)
 
-    def cambiar_cuadro(self, clase_cuadro: grafica.CTkFrame):
-        """
-           Función que destruye un cuadro (Clase Frame) y 
-           crea otro cuadro para reemplazarlo.
 
-           args:
-                 clase_cuadro: Clase CTkFrame/TkFrame
-           devuelve:
-
-           excepciones:
-        """
-        #Crea un nuevo cuadro a partir de la clase cuadro
-        nuevo_cuadro = clase_cuadro(self)
-        #Si ya existe un cuadro, este es destruido y reemplazado por el nuevo
-        if self._cuadro is not None:
-            self._cuadro.destroy()
-        #reemplazar el cuadro destruido por el nuevo cuadro
-        self._cuadro = nuevo_cuadro
-        #Colocar el cuadro en la pantalla
-        self._cuadro.grid()
+    def mostrar_ventana_panel(self):
+        if not self.panel_referencia:
+            self.panel_referencia = pn.Panel()
+        self.panel_referencia.show()
+        self.hide()
